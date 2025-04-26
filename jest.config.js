0d9dea1ebfc69184fc3be481270d36fe0847e26c
cliffhall <cliff@futurescale.com>
2025-04-26 15:07:30 -0400

    Intitial commit

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    testMatch: ["**/?(*.)+(spec|test).ts?(x)"],
+    maxWorkers: 11,
+    coverageDirectory: 'coverage',
+    collectCoverage: true,
+    collectCoverageFrom: ['src/**/*.ts']
+
+};
