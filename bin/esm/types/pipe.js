b77bff4310e507484d752c404998365b62b29801
cliffhall <cliff@futurescale.com>
2025-12-17 19:33:58 -0500

    Remove bin/ and add to .gitignore

--- bin/esm/types/pipe.js
+++
@@ -1,8 +0,0 @@
-/**
- * Accept input pipe notification name constant.
- */
-export const ACCEPT_INPUT_PIPE = "acceptInputPipe";
-/**
- * Accept output pipe notification name constant.
- */
-export const ACCEPT_OUTPUT_PIPE = "acceptOutputPipe";
